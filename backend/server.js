00feb48e948858ff6437df1c20c3a1ab7ad43654
Akshatt10 <akshattyagi1010@gmail.com>
2024-11-28 03:40:20 +0530

    Updated package.json and server.js

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,15 @@ import connectDB from './db/connectDB.js';
 import "./passport/github.auth.js";
 import passport from 'passport';
 import session from 'express-session';
-
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
@@ -20,18 +24,22 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-});
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
 // Connect to the database first
+app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
+
 connectDB().then(() => {
-    app.listen(5000, () => {
-        console.log('Server started on http://localhost:5000');
+    app.listen(PORT, () => {
+        console.log(`Server started on http://localhost:${PORT}`);
     });
 }).catch((error) => {
     console.error("Failed to start server due to DB connection issues:", error);
